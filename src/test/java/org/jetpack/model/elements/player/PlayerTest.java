2d053bace17b1a4db528304bea8f746c1a78fe85
Catarina Pires <catarina.o.pires@hotmail.com>
2021-05-29 12:26:18 +0100

    Added more tests to Player

--- src/test/java/org/jetpack/model/elements/player/PlayerTest.java
+++ src/test/java/org/jetpack/model/elements/player/PlayerTest.java
@@ -1,5 +1,8 @@
 package org.jetpack.model.elements.player;
 
+import net.jqwik.api.ForAll;
+import net.jqwik.api.Property;
+import net.jqwik.api.constraints.Positive;
 import org.jetpack.model.Position;
 import org.jetpack.model.elements.player.playerStrategies.PlayerStrategy;
 import org.junit.jupiter.api.Test;
@@ -31,5 +34,33 @@ class PlayerTest {
         assertTrue(player.buyPowerUp());
         // Power-Up costs 10 (20-10=10 coins left)
         assertEquals(player.getCoins(), 10);
+    }
+
+    @Property
+    void takeDamage(@ForAll @Positive int damage) {
+        Position position = Mockito.mock(Position.class);
+        Player player = new Player(position);
+        int initialLives = player.getLives();
+
+        PlayerStrategy strategy = Mockito.mock(PlayerStrategy.class);
+        Mockito.when(strategy.damageTaken()).thenReturn(damage);
+        player.setStrategy(strategy);
+
+        player.takeDamage();
+        assertEquals(player.getLives() + damage, initialLives);
+    }
+
+    @Property
+    void addCoin(@ForAll @Positive int coin) {
+        Position position = Mockito.mock(Position.class);
+        Player player = new Player(position);
+        int initialCoins = player.getCoins();
+
+        PlayerStrategy strategy = Mockito.mock(PlayerStrategy.class);
+        Mockito.when(strategy.coinTaken()).thenReturn(coin);
+        player.setStrategy(strategy);
+
+        player.addCoin();
+        assertEquals(player.getCoins() - coin, initialCoins);
     }
 }
