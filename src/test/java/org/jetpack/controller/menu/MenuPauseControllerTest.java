c5688c1b831827514c55037aa22bc726a4e48893
adr1an0s0ar3s <adrianosoares35@gmail.com>
2021-05-30 15:59:32 +0100

    Updated tests :pencil2:

--- src/test/java/org/jetpack/controller/menu/MenuPauseControllerTest.java
+++ src/test/java/org/jetpack/controller/menu/MenuPauseControllerTest.java
@@ -3,6 +3,7 @@ package org.jetpack.controller.menu;
 import org.jetpack.controller.GameLoop;
 import org.jetpack.gui.GUI;
 import org.jetpack.gui.LanternaGUI;
+import org.jetpack.model.arena.RandomArenaBuilder;
 import org.jetpack.model.menu.PauseMenu;
 import org.jetpack.states.*;
 import org.junit.jupiter.api.BeforeEach;
@@ -29,5 +30,13 @@ class MenuPauseControllerTest {
         gameLoop.setState(new PauseMenuState(new PauseMenu(), Mockito.mock(ArenaState.class)));
         gameLoop.run();
         assertTrue(gameLoop.getState() instanceof MainMenuState);
+    }
+
+    @Test
+    void transitionFromArenaToPause() throws IOException {
+        Mockito.when(gui.getNextAction()).thenReturn(GUI.ACTION.PAUSE, GUI.ACTION.QUIT);
+        gameLoop.setState(new ArenaState(new RandomArenaBuilder(10,10).createArena()));
+        gameLoop.run();
+        assertTrue(gameLoop.getState() instanceof PauseMenuState);
     }
 }
